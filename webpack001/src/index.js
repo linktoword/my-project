ca4c90e446f645abf1ef2c72887001c4b5152117
ZSF <37545695+linktoword@users.noreply.github.com>
2020-06-11 08:12:37 +0800

    清理项目未下一章做准备

--- webpack001/src/index.js
+++ webpack001/src/index.js
@@ -4,20 +4,13 @@
  * @Date: 2020-06-10 22:20:14
  */ 
 import _ from 'lodash';
-import './style.css';
-import Icon from './mypng.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  let myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+  
   return element;
 }
 
