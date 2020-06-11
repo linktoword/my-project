403719896baf50b83518a2c35589aeeb34571782
ZSF <37545695+linktoword@users.noreply.github.com>
2020-06-11 20:45:17 +0800

    完成管理输出官网例子

--- webpack001/src/index.js
+++ webpack001/src/index.js
@@ -4,12 +4,16 @@
  * @Date: 2020-06-10 22:20:14
  */ 
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.onclick = printMe;
+  element.appendChild(btn);
   
   return element;
 }
--- webpack001/webpack.config.js
+++ webpack001/webpack.config.js
@@ -4,11 +4,22 @@
  * @Date: 2020-06-11 07:04:28
  */ 
 const path = require('path');
+const HtmlWekpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWekpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
